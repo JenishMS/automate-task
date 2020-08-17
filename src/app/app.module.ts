95ce9cd23e4f5e891a7bdbd8fb7a3e8361b82ff9
Jenish <jenishmstephen@gmail.com>
2020-08-17 22:41:15 +0530

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModuleModule } from './shared/material-module.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { MenusComponent } from './components/menus/menus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    MenusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
