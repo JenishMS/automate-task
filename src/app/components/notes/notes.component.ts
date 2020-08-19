7c4fc63b15dc90dc950c8e0196750f43311387d4
Jenish <jenishmstephen@gmail.com>
2020-08-19 10:04:59 +0530

    completed responsive

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 import { NotesService } from 'src/app/services/notes.service';
 import { Note } from 'src/app/models/note.model';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { MatDrawer } from '@angular/material/sidenav';
+
+const SMALL_WIDTH_BRAEKPOINT = 720;
 
 @Component({
   selector: 'app-notes',
@@ -10,11 +15,27 @@ import { Note } from 'src/app/models/note.model';
 export class NotesComponent implements OnInit {
 
   note: Note;
+  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BRAEKPOINT}px)`);
+  @ViewChild(MatDrawer) drawer: MatDrawer;
 
-  constructor(private noteServ: NotesService) { }
+  constructor(private noteServ: NotesService, private router: Router, zone: NgZone) {
+    this.mediaMatcher.addListener(mql => {
+      if(mql.matches) {
+        this.drawer.close();
+      }else if(this.drawer.opened === false){
+        this.drawer.open();
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.noteServ.fetchNotes();
+
+    this.router.events.subscribe( () => {
+      if(this.isScreenSmall()) {
+        this.drawer.close();
+      }
+    });
   }
 
   /**
@@ -39,5 +60,9 @@ export class NotesComponent implements OnInit {
     if(this.note) {
       this.noteServ.deleteNote(this.note.noteId);
     }
+  }
+
+  isScreenSmall():boolean {
+    return this.mediaMatcher.matches;
   }
 }
